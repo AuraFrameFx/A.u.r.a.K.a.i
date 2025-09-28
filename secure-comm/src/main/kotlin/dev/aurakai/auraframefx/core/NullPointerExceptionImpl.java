813f2a5b5e6401f2db31427464740ed5a3de9ea4
AuraKai <wehttam1989@gmail.com>
2025-09-27 21:48:07 -0600

    Update build config, ProGuard, manifest, and add exception class
    Downgraded Java and JVM target versions in build.gradle.kts, updated Firebase and Material dependencies, and applied Crashlytics plugin. Added ProGuard rules to keep MainActivity and all activities. Included SCHEDULE_EXACT_ALARM permission in AndroidManifest.xml. Added NullPointerExceptionImpl class to core module.

---
+++ secure-comm/src/main/kotlin/dev/aurakai/auraframefx/core/NullPointerExceptionImpl.java
@@ -0,0 +1,7 @@
+package dev.aurakai.auraframefx.core;
+
+public class NullPointerExceptionImpl extends NullPointerException {
+    public NullPointerExceptionImpl(String s) {
+        super(s);
+    }
+}
