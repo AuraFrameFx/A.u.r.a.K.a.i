da77d934967eba7ee9d4d29f3bc55b1fc6c1aa74
AuraKai <wehttam1989@gmail.com>
2025-10-05 17:35:38 -0600

    refactor: Consolidate Android application plugins and fix Hilt configuration
    This commit refactors the build logic by moving common Android application plugins into the `AndroidApplicationConventionPlugin`.
    
    **Key Changes:**
    *   The `AndroidApplicationConventionPlugin` now automatically applies the following plugins:
        *   `org.jetbrains.kotlin.android`
        *   `org.jetbrains.kotlin.plugin.compose`
        *   `com.google.devtools.ksp`
        *   `com.google.dagger.hilt.android`
    *   In the `app` module's `build.gradle.kts`, the Hilt compiler dependency is correctly moved from the `implementation` configuration to `ksp` to align with modern best practices.

--- build-logic/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -25,7 +25,10 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
                 apply("org.jetbrains.kotlin.plugin.compose")
+                apply("com.google.devtools.ksp")
+                apply("com.google.dagger.hilt.android")
             }
 
             extensions.configure<ApplicationExtension> {
