dbbb89d21980dbd47aec9433c59ca9a58ce82783
AuraKai <wehttam1989@gmail.com>
2025-10-05 11:05:10 -0600

    Update settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,4 +1,6 @@
-// ==== GENESIS PROTOCOL - BUILD LOGIC SETTINGS ====
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
